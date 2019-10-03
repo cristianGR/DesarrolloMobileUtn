c47a1caded8679b0a8a2f46b2cb809d77482fefb
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-10-03 00:00:28 -0300

    Programa terminado

--- EJN1/ConsoleAppAndIf/ConsoleAppAndIf/Program.cs
+++ EJN1/ConsoleAppAndIf/ConsoleAppAndIf/Program.cs
@@ -10,21 +10,38 @@ namespace ConsoleAppAndIf
         static void Main(string[] args)
         {
             int numeroIngresado = 0;
-            String mensajeAlOperador = "";
+            String mensajeAlOperador = "Escriba un numero: ";
 
             try
             {
-
+                Console.WriteLine(mensajeAlOperador);
+                numeroIngresado = int.Parse(Console.ReadLine());
             }
-            catch
+            catch(FormatException)
             {
-
+                mensajeAlOperador = "Ingrese un número entero por favor. Se tomará por defecto el 0";
+                Console.WriteLine(mensajeAlOperador);
             }
             finally
             {
-
+                mensajeAlOperador = EvaluarEntreNrosAnd(0,100, numeroIngresado)?$"El valor {numeroIngresado} es válido":
+                    $"El valor {numeroIngresado} no es válido";
+                Console.WriteLine(mensajeAlOperador);
+           
             }
 
+        }
+
+        private static bool EvaluarEntreNrosAnd(int valorCotaMenor, int valorCotaMayor, int valorIngresado)
+        {
+            return (valorIngresado > valorCotaMenor && valorIngresado < valorCotaMayor) ? true : false;
+        }
+
+        private static bool EvaluarEntreNrosIf(int valorCotaMenor, int valorCotaMayor, int valorIngresado)
+        {
+            if (valorIngresado > valorCotaMenor)
+                return (valorIngresado < valorCotaMayor) ? true : false;
+            return false;
         }
     }
 }
