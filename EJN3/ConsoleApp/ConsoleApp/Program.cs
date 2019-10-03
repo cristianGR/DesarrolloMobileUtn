51f04c60bf7a81287513a8e004c5e7bc1c24b901
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-10-03 02:02:47 -0300

    Calculadora

--- EJN3/ConsoleApp/ConsoleApp/Program.cs
+++ EJN3/ConsoleApp/ConsoleApp/Program.cs
@@ -6,7 +6,60 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numeros = new int[100];
+            char[] operadores = new char[100];
+            int seleccion = 0; 
+            String mensajeAlOperador = "";
+
+            ConsoleKeyInfo input;
+
+            do
+            {
+                Console.Clear();
+                mensajeAlOperador = "Este es un programa que sirve como:\n" +
+                    "CALCULADORA(0) -soportando : SUMA (+), RESTA (-) , MULTIPLICACIÓN (*) , DIVISIÓN (/)\n" +
+                    "SUPERFICIE (1)\n" +
+                    "PERIMETRO (2)\n"+
+                    "ESC(salir)\n";
+                Console.WriteLine(mensajeAlOperador);
+                seleccion = int.Parse(Console.ReadLine());
+
+                switch (seleccion)
+                {
+                    case 0:
+                        CalcularExpresion();
+                        break;
+                    case 1:
+                        CalcularSuperficie();
+                        break;
+                    case 2:
+                        CalcularPerimetro();
+                        break;
+                    default:
+                        mensajeAlOperador = "Escriba opción válida";
+                        Console.WriteLine(mensajeAlOperador);
+                        break;
+                }
+
+                mensajeAlOperador = "Desea salir? [ESC]";
+                Console.WriteLine(mensajeAlOperador);
+                input = Console.ReadKey();
+            } while (input.Key != ConsoleKey.Escape);
+        }
+
+        private static void CalcularPerimetro()
+        {
+            Console.WriteLine("Perimetro");
+        }
+
+        private static void CalcularSuperficie()
+        {
+            Console.WriteLine("Superficie");
+        }
+
+        private static void CalcularExpresion()
+        {
+            Console.WriteLine("Calculadora");
         }
     }
 }
