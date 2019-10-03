1f0b50db849f4a5e80fc8b19137e30c11197cffe
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-10-03 17:12:44 -0300

    Factorial

---
+++ ArrayEJN2/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nroPrueba = 0;
+            Random nroAleatorio = new Random();
+            nroPrueba = nroAleatorio.Next(10);
+            Console.Write($"El numero a Factorizar fue: {nroPrueba} y el resultado fue: ");
+            Console.WriteLine(Factorial(nroPrueba));
+        }
+
+        static int Factorial(int nro)
+        {
+            return (nro == 1)? 1:(nro * Factorial(nro - 1));
+        }
+    }
+}
