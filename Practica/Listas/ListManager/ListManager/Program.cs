86535307d7317e557bf9a5c4e03f066d1dacbdb9
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-10-08 23:30:32 -0300

    Lista

---
+++ Practica/Listas/ListManager/ListManager/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MathExpresion = new List<String> { "(", "(", "3", "+", "4", ")", "*", "6", ")" };
+            var index = MathExpresion.IndexOf("(");
+            int count = 0;
+
+            foreach (var exp in MathExpresion)
+            {
+                if (exp.IndexOf("(") == 0)
+                {
+                    Console.WriteLine($"( se encuentra  en la posición {count}");
+                }
+                if (exp.IndexOf(")") == 0)
+                {
+                    Console.WriteLine($") se encuentra  en la posición {count}");
+                }
+                count++;
+            }
+
+            //Console.WriteLine("------" +MathExpresion.IndexOf("(")); si no se recorre con un foreach no muestra todoas las ocurrencias.
+            //IndexOf si lo encuentra devuelve cero y sino -1.
+        }
+    }
+}
