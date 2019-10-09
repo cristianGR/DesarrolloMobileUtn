64a32500133286b379db92b35b41bc412003e2e6
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-10-09 01:19:21 -0300

    Manejo de lista

--- Practica/Listas/ListManager/ListManager/Program.cs
+++ Practica/Listas/ListManager/ListManager/Program.cs
@@ -7,25 +7,31 @@ namespace ListManager
     {
         static void Main(string[] args)
         {
-            var MathExpresion = new List<String> { "(", "(", "3", "+", "4", ")", "*", "6", ")" };
-            var index = MathExpresion.IndexOf("(");
-            int count = 0;
+            var MathExpresion = new List<string>{ "(", "(", "3", "+", "4", ")", "*", "6", ")" };
+            var QueueParentesisCierre = new Queue<string>();
+            var StackParentesisApertura = new Stack<string>();
+            string count = "0";
+            int i = 0;
 
             foreach (var exp in MathExpresion)
             {
                 if (exp.IndexOf("(") == 0)
                 {
-                    Console.WriteLine($"( se encuentra  en la posición {count}");
+                    //Console.WriteLine($"( se encuentra  en la posición {count}");
+                    StackParentesisApertura.Push(count);
                 }
                 if (exp.IndexOf(")") == 0)
                 {
-                    Console.WriteLine($") se encuentra  en la posición {count}");
+                    // Console.WriteLine($") se encuentra  en la posición {count}");
+                    QueueParentesisCierre.Enqueue(count);
                 }
-                count++;
+                count = "" + i++;
             }
 
-            //Console.WriteLine("------" +MathExpresion.IndexOf("(")); si no se recorre con un foreach no muestra todoas las ocurrencias.
-            //IndexOf si lo encuentra devuelve cero y sino -1.
+
+            Console.WriteLine($"1° intervalo de solución : [{StackParentesisApertura.Pop()}],[{QueueParentesisCierre.Dequeue()}]");
+            Console.WriteLine($"2° intervalo de solución: [{StackParentesisApertura.Pop()}],[{QueueParentesisCierre.Dequeue()}]");
+
         }
     }
 }
